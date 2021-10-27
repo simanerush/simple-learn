90ab63c9e196068805593ffcd12e0d0b301a5a81
Sima Nerush <2002ssn@gmail.com>
2021-10-27 16:24:30 -0700

    Initial Commit

---
+++ SmartLearn/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SmartLearn
+//
+//  Created by Serafima on 10/27/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
