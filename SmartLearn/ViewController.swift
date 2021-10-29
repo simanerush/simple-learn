7eea0d38cab3f115c6116b40e33145228732ca04
Sima Nerush <2002ssn@gmail.com>
2021-10-29 14:33:35 -0700

    Get a textField value

--- SmartLearn/ViewController.swift
+++ SmartLearn/ViewController.swift
@@ -8,12 +8,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var textToLearn: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        textToLearn.delegate = self
+        textToLearn.returnKeyType = .done
     }
 
+}
+
+extension ViewController: UITextFieldDelegate {
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        print(textField.text ?? "no text")
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
 
 }
 
